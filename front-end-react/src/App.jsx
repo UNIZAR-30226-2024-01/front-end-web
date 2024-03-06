5a05de5f9aba25006707d6e080eeab2197366df3
Javier Sin Pelayo <843442@unizar.es>
2024-03-06 20:41:24 +0100

    inicio gestión enrutamiento
    Se han añadido comentarios explicativos de la estructura básica del enrutamiento a seguir. Además se ha cambiado el englobado de la app de React.StrictMode a BrowserRouter para permitir dicho enrutamiento.

--- front-end-react/src/App.jsx
+++ front-end-react/src/App.jsx
@@ -2,13 +2,42 @@ import '../../../front-end-shared/css/App.css'
 import {Tarjeta} from './components/Tarjeta/Tarjeta.jsx'
 import {Chat} from './components/Chat/Chat.jsx'
 import {Tablero} from './components/Tablero/Tablero.jsx'
+// import { Routes } from 'react-router-dom'
+// import { Route } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 // import { Carta } from './components/Carta.jsx'
 
+/****** 👇🏼 ROUTES 👇🏼 ****** 
+# Not logged--> create account (username + password) or login
+# Logged: 
+  -no game started
+    -starting game (loading screen waiting for other players)
+    -in-game (new game)
+  -game started
+    -in-game (waiting for other players to join)
+**************************/
 
 function App() {
   
   return (
     <>
+      {/* 
+          If there's something you want to render in every page, put it here 
+          The rest of the routing rendering will be inside 'Routes'
+
+          Use 'Link' to navigate between pages instead of 'a' tags because 'a' tags will refresh the page
+          and 'Link' will only change the URL (fast and smooth navigation between pages)
+      */}
+      
+
+    {/* 
+      <Routes>
+        <Route path='/' element={<LoginOrRegister />}/>
+        ...
+        Add the rest of routes
+      </Routes> 
+    */}
+
       <Tarjeta />
       <Chat />
       < Tablero />
