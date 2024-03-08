908f1a108999c69dd3c27d3a0af0e0a465cb391c
luoolmos <146450799+luoolmos@users.noreply.github.com>
2024-03-08 13:13:42 +0100

    chat changes

--- front-end-react/src/components/Chat/Chat.jsx
+++ front-end-react/src/components/Chat/Chat.jsx
@@ -1,18 +1,51 @@
 import '../../../../../front-end-shared/css/Chat/chat.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { MessageList } from './MessageList.jsx';
 import { InputMessage } from './InputMessage.jsx';
 import { Desplegable } from '../Desplegable.jsx';
+// import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js';
+import { socket } from './chat.js';
+
+
 
 export function Chat (){
     const [messages, setMessages] = useState([]);
-    
     const [desplegable, setDesplegable] = useState(false)
     const style = { left: `${desplegable ? '0px' : '-425px' }`} 
 
     const sendMessage = (message) => {
-        setMessages([...messages, message]);
+        console.log('Sending message:', message);
+        socket.auth.username = sessionStorage.getItem('username') ?? 'anonymous';
+        socket.auth.group = sessionStorage.getItem('game') ?? '0';
+        console.log('group :', socket.auth.group);
+        socket.emit('chat message', message);
     };
+
+    useEffect(() => {
+        socket.auth.username = sessionStorage.getItem('username') ?? 'anonymous';
+        socket.auth.group = sessionStorage.getItem('group') ?? '0';
+        socket.connect();
+
+        const onConnect = () => {
+            console.log('Connected to server');
+        } 
+    
+        const onChatResponse = (username, message) => {
+            console.log('Received message:', message);
+            const messageToShow = `${username} sent: ${message}`
+            setMessages((prevMessages) => [...prevMessages, messageToShow]);
+        };
+    
+        socket.on('connect', onConnect);
+        socket.on('chat response', onChatResponse);
+    
+        return () => {
+          socket.off('connect', onConnect);
+          socket.off('chat response', onChatResponse);
+          socket.disconnect();
+        };
+      }, []);
+    
 
 
     return (
