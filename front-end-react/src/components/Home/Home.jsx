baedb98653f4a1cbd5ff669a2987f03b4f254322
Javier Sin Pelayo <843442@unizar.es>
2024-03-18 17:34:28 +0100

    Avances del home
    Avances en la disposición del menú de acuerdo a bocetos establecidos en Excalidraw

--- front-end-react/src/components/Home/Home.jsx
+++ front-end-react/src/components/Home/Home.jsx
@@ -1,45 +1,30 @@
 import { ProgressBar } from "./ProgressBar"
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import '../../../../../front-end-shared/css/Home/Home.css'
 
 export function Home() {
 
-    const [completed, setCompleted] = useState(100)
-    const [rotate, setRotate] = useState(false)
+    const [completed, setCompleted] = useState(69)
 
-    // Función de prueba para movimiento automático de la barra de progreso 
-    useEffect(() => {
-        if (!rotate) return;
-        const interval = setInterval(() => setCompleted(
-            (prevCompleted) => (prevCompleted >= 100 ? 0 : prevCompleted + 10)
-        ), 800);
-        return () => clearInterval(interval);
-    }, [rotate]);
-
-    const click = () => {
-        setRotate((prevRotate) => !prevRotate)
-    }
-
-    const clickAdd = () => {
-        setCompleted((prevCompleted) => (prevCompleted >= 100 ? 0 : prevCompleted + 10))
-    }
-    const clickSubtract = () => {
-        setCompleted((prevCompleted) => (prevCompleted <= 0 ? 100 : prevCompleted - 10))
-    }
-  
     return (
-        <div>
-            <ProgressBar completed={completed}/>
-            <div>
-                <button onClick={click}>
-                    {rotate ? 'Stop' : 'Start'}
-                </button>
-                <button onClick={clickAdd}>
-                    Add 10
-                </button>
-                <button onClick={clickSubtract}>
-                    Subtract 10
-                </button>
-            </div>
+        <div className="home-root">
+            <section className="aux">
+                <div className="home-username">
+                    <p>alexelcapo</p>
+                </div>
+                <ProgressBar completed={completed}/>
+            </section>
+
+            <section className="home-body">
+                <div className="provisional-board-image"/>
+
+                <aside className="gameModes">
+                    {/* TO-DO: Cuando clicas Singleplayer o Multiplayer, desaparecen
+                    y después salen: 'Partida nueva', 'Unirse a partida' */}
+                    <button className="gamemode-button">Solitario</button>
+                    <button className="gamemode-button">Multijugador</button>
+                </aside>
+            </section>
         </div>
     )
 }
