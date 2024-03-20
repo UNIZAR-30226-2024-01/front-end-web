6f4f9f29caf7b905bf937d3a884bf0bb0e46406d
841723 <841723@unizar.es>
2024-03-20 20:08:05 +0100

    update socket connect URL

--- front-end-react/src/components/Game/Chat/chat.js
+++ front-end-react/src/components/Game/Chat/chat.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://51.20.246.74:3000';
 
 export const socket = io(URL, {
     auth: {
