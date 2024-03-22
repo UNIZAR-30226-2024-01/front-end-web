062252d70180432c6a50531b794c1abd2654b581
841723 <841723@unizar.es>
2024-03-22 19:12:09 +0100

    despliegue con conexion dinamica
    cambia en endpoint backend en funcion si se despliega en prodution o en development

---
+++ front-end-react/build-www.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home/ubuntu/grace-hopper-01/front-end-web/front-end-react
+
+export PATH=/home/ubuntu/.nvm/versions/node/v21.7.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+npm run build-prod
+echo 'build completado'
+
+sudo rm -r /var/www/dist
+sudo mv dist /var/www/
