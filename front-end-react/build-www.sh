dc0870b3b8736563d4baa96abd67d886957733c3
841723 <841723@unizar.es>
2024-03-22 19:37:35 +0100

    Update build-www.sh
    añade npm i antes de compilarlo

--- front-end-react/build-www.sh
+++ front-end-react/build-www.sh
@@ -3,6 +3,7 @@
 cd /home/ubuntu/grace-hopper-01/front-end-web/front-end-react
 
 export PATH=/home/ubuntu/.nvm/versions/node/v21.7.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+npm i
 npm run build
 echo 'build completado'
 
